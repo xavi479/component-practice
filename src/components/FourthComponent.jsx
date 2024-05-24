f6abdcfdb61909de80da672fef326b7ae1542762
inesmariao <inesmao.misena@gmail.com>
2024-05-23 21:10:45 -0500

    Events Mouse with console.log and alert

--- src/components/FourthComponent.jsx
+++ src/components/FourthComponent.jsx
@@ -9,13 +9,23 @@ export const FourthComponent = () => {
   const handleDoubleClicked = (e) => {
     alert("Has hecho doble click al botón");
   }
-
-  const handleMouseEnter = (e) => {
+  // onMouseEnter y onMouseLeave con dos funciones
+    const handleMouseEnter = (e) => {
     console.log("Has entrado a mi caja con el Mouse!! ")
   }
 
   const handleMouseLeave = (e) => {
     console.log("Has salido de mi caja!! ")
+  }
+
+  // onMouseEnter y onMouseLeave con una sola función con Alert
+  const handleMouse = (e, action) => {
+    alert(`Has ${action}  la caja`);
+  }
+
+  // onMouseEnter y onMouseLeave con una sola función con  Console Log
+  const handleMouseCl = (e, action) => {
+    console.log(`Has ${action}  la caja`);
   }
 
   return (
@@ -38,11 +48,25 @@ export const FourthComponent = () => {
 
         {/* Evento Mouse Enter y Mouse Leave */}
         <hr />
-        <div id="box"
-          onMouseEnter={ handleMouseEnter }
-          onMouseLeave={ handleMouseLeave }
-        >
-          <p>Pasa el Mouse por encima!!!</p>
+        <div id="box">
+          <div id="box1"
+            onMouseEnter={ handleMouseEnter }
+            onMouseLeave={ handleMouseLeave }
+          >
+            <p>Pasa el Mouse por encima!!!</p>
+          </div>
+          <div id="box2"
+            onMouseEnter={ e => handleMouse(e, "entrado a") }
+            onMouseLeave={ e => handleMouse(e, "salido de") }
+          >
+            <p>Pasa el Mouse por encima!!!</p>
+          </div>
+          <div id="box3"
+            onMouseEnter={ e => handleMouseCl(e, "entrado a") }
+            onMouseLeave={ e => handleMouseCl(e, "salido de") }
+          >
+            <p>Pasa el Mouse por encima!!!</p>
+          </div>
         </div>
         {/* Evento Focus y Blur */}
 
