36dc38b8cfcbcd866537dbb9b714a00c17905e2f
inesmariao <inesmao.misena@gmail.com>
2024-05-23 20:27:20 -0500

    Event onDoubleClick

--- src/components/FourthComponent.jsx
+++ src/components/FourthComponent.jsx
@@ -6,6 +6,10 @@ export const FourthComponent = () => {
     alert("Has hecho click al botón!!");
   }
 
+  const handleDoubleClicked = (e) => {
+    alert("Has hecho doble click al botón");
+  }
+
   return (
     <div>
         <h2>Eventos en React</h2>
@@ -19,8 +23,10 @@ export const FourthComponent = () => {
           <button onClick={ handleClicked }>Aquí también haz Click!!</button>
         </div>
 
-
         {/* Evento Doble Click */}
+        <div>
+          <button onDoubleClick={ handleDoubleClicked }>Haz doble click!!</button>
+        </div>
         {/* Evento Mouse Enter y Mouse Leave */}
         {/* Evento Focus y Blur */}
 
